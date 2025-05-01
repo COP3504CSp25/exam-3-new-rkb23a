fdaf6d7b11e703e94f20a6c6d69151de1580f91a
rkb23a <rachelburns@ufl.edu>
2025-05-01 20:08:30 +0000

    oh i just needed a comment

--- TrafficData.hpp
+++ TrafficData.hpp
@@ -44,21 +44,21 @@ number, according to the CSV file.
 */
 void updateTrafficData(const std::string& filename, std::map<std::string, int>& dataMap) {
     
-  std::ifstream in(filename);
-  if (!in){
-    return;
-  }
+  // std::ifstream in(filename);
+  // if (!in){
+  //   return;
+  // }
 
-  std::map<std::string, int> trafficData = loadTrafficData(filename);
+  // std::map<std::string, int> trafficData = loadTrafficData(filename);
 
-  auto it = dataMap.begin();
-  while (it != dataMap.end()){
-    if (dataMap[it->first]){
-      dataMap[it->first] = it->second;
-        }
-    else
-    dataMap.insert({it->first, it->second});
-  }
+  // auto it = dataMap.begin();
+  // while (it != dataMap.end()){
+  //   if (dataMap[it->first]){
+  //     dataMap[it->first] = it->second;
+  //       }
+  //   else
+  //   dataMap.insert({it->first, it->second});
+  // }
 
 
   }
